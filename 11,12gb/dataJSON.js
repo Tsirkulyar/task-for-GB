63823b9ab0ec93bc4b080daa24672e3b9e0c0dd2
tsirkulyar <106337353+Tsirkulyar@users.noreply.github.com>
2023-02-19 18:00:12 +0300

    Add files via upload

---
+++ 11,12gb/dataJSON.js
@@ -0,0 +1,38 @@
+const dataCard = `[
+    {
+        "photo": "/img/Rectangle 15 copy (1).png",
+        "name": "ELLERY X M'O CAPSULE",
+        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "$52.00"
+    },
+    {
+        "photo": "/img/Layer 51.png",
+        "name": "2ELLERY X M'O CAPSULE",
+        "text": "2Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "2$52.00"
+    },
+    {
+        "photo": "/img/Rectangle 15 copy (2).png",
+        "name": "3ELLERY X M'O CAPSULE",
+        "text": "3Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "3$52.00"
+    },
+    {
+        "photo": "/img/Rectangle 15 copy (3).png",
+        "name": "4ELLERY X M'O CAPSULE",
+        "text": "4Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "4$52.00"
+    },
+    {
+        "photo": "/img/Rectangle 15 copy (4).png",
+        "name": "5ELLERY X M'O CAPSULE",
+        "text": "5Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "5$52.00"
+    },
+    {
+        "photo": "/img/Rectangle 15 copy (5).png",
+        "name": "6ELLERY X M'O CAPSULE",
+        "text": "6Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "6$52.00"
+    }
+]`
